8754b3c35f08b91c8874cd74ca2c90c08367e2e1
Pushpalatha Chimmili <pushpalatha.chimmili@sasi.ac.in>
2025-09-19 12:05:05 +0530

    my commit

---
+++ exp1/1.dart
@@ -0,0 +1,13 @@
+void main() {
+  int a = 10;
+  int b = 5;
+
+  print("a = $a");
+  print("b = $b");
+
+  print("Addition: ${a + b}");
+  print("Subtraction: ${a - b}");
+  print("Multiplication: ${a * b}");
+  print("Division: ${a / b}");
+  print("Modulus (Remainder): ${a % b}");
+}
